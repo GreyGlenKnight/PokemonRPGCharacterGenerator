c083e24c5017c0a0984d048706096ac310903242
LuciusWooding <rhakiath@mac.com>
2018-04-01 00:05:41 -0400

    Char sheet Spawner
    Spawning/physics script for background sprites.

--- PokemonRPGCharacterGenerator/Assets/Prefabs/PokeballSpawner.cs
+++ PokemonRPGCharacterGenerator/Assets/Prefabs/PokeballSpawner.cs
@@ -2,49 +2,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
-using System;
 
 public class PokeballSpawner : MonoBehaviour
 {			
+	private int NumberSpawned;
 	public GameObject PortraitObject;
 	public GameObject Pokedoll;
 	public Sprite Charmander;
 	public Rigidbody2D CharCol;
 	public Canvas BGCanvas;
 	public Vector3 MousePosition;
-	List<GameObject> Population = new List<GameObject> ();
-//	BoxCollider2D Background;
+	public Vector3 NewPos;
+	GameObject[] Population = new GameObject[64];
+	//	BoxCollider2D Background;
 
 	public void Spawn ()
-
 	{
 		//PortraitObject = this;
-
-		//PortraitObject = new GameObject();
-
-		Instantiate (Pokedoll, MousePosition, Quaternion.identity);
-		Population.Add (Pokedoll);
-		Debug.Log (MousePosition);
-		Debug.Log (Population.Count);
-		var i = Population.Count;
-		if(i>6)
-
-		{ 
-			Destroy(Population[i]);
-			i--;
-			Debug.Log ("Culling...");
-
+		Vector3 NewPos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		NewPos.z = 1;
+		if (Population [NumberSpawned % 64] != null) 
+		{
+			Destroy (Population [NumberSpawned % 64]);
 		}
-		}
-		//GameObject Pokedoll = GameObject.Instantiate (PortraitObject, Vector3(transform.position.x,transform.position.y, transform.position.z));// as GameObject;
-		//Pokedoll.transform.parent = BGCanvas.transform;
-		//Instantiate(box,Vector3 (Input.mousePosition.x,0,Input.mousePosition.z), Quaternion.identity); } 
-		//PortraitObject = GameObject.Instantiate (PortraitObject, Input.mousePosition, transform.rotation) as GameObject;
-	
-		//PortraitObject.AddComponent<Rigidbody2D>();
 
-
-
+		Population [NumberSpawned % 64] = Instantiate (Pokedoll, NewPos, Quaternion.identity);
+		NumberSpawned++;
+	}
 
 	void Update()
 	{
@@ -52,6 +36,8 @@ public class PokeballSpawner : MonoBehaviour
 		if (Input.GetMouseButtonUp (0)) 
 		{
 			MousePosition = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0f);
+		
+			//audioSource.pitch = rng;
 			Spawn ();
 		
 				//DestroyObject (Pokedoll);
@@ -61,3 +47,33 @@ public class PokeballSpawner : MonoBehaviour
 		}
 	
 }
+
+
+//GameObject newPokedoll = Instantiate (Pokedoll, NewPos, Quaternion.identity);
+//	Population.Add (newPokedoll);
+//GameObject Before Instantiate new Pokedoll =  //(Pokedoll, MousePosition, Quaternion.identity);
+//Population.Add (new Pokedoll);
+//	Debug.Log (MousePosition);
+//	Debug.Log (Population.Count);
+//	var i = Population.Count;
+//if(i>64)
+//{
+//	Destroy(Population[i-1]);
+
+//	Population.RemoveAt(Population.Count - 1);
+//		i--;
+//		Debug.Log ("Culling...");
+
+//	}
+
+
+
+//GameObject Pokedoll = GameObject.Instantiate (PortraitObject, Vector3(transform.position.x,transform.position.y, transform.position.z));// as GameObject;
+//Pokedoll.transform.parent = BGCanvas.transform;
+//Instantiate(box,Vector3 (Input.mousePosition.x,0,Input.mousePosition.z), Quaternion.identity); } 
+//PortraitObject = GameObject.Instantiate (PortraitObject, Input.mousePosition, transform.rotation) as GameObject;
+
+//PortraitObject.AddComponent<Rigidbody2D>();
+
+
+
